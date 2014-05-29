f6df814e1929c4e010dde8a85e40a389ddd3890f
Andrew Sheelan Paliyan <andrew@yankist.com>
2014-05-29 07:56:51 -0400

    Added files

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/staten/"
+http_images_path = "/staten/images"
+http_generated_images_path = "/staten/images"
+http_fonts_path = "/staten/fonts"
+css_dir = "/staten/public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
